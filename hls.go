8a274b5df1bad3b343ff892e684126017828f276
Otto Modinos <ottomodinos@gmail.com>
2018-12-11 03:01:30 +0200

    add context, rename Get to Download
    It's not perfect since I'm using "standard" io.Copy which doesn't do
    context but it's better and I can have timeouts.
    
    The deadlock should be gone, yay!  Even without context it could still
    manifest if io.Copy errored out so this commit makes everything more or
    less deal with errors correctly, always returning them.

--- hls.go
+++ hls.go
@@ -1,13 +1,14 @@
 package godam
 
 import (
+	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"sort"
 	"time"
 
 	"github.com/grafov/m3u8"
@@ -23,11 +24,29 @@ func (e HTTPError) Error() string {
 }
 
 type HLS struct {
-	Client *http.Client
+	Client        *http.Client
+	SelectVariant func([]*m3u8.Variant) *m3u8.Variant
 }
 
-func getBestBandwidth(u *url.URL) (*m3u8.Variant, error) {
-	r, err := http.Get(u.String())
+func sleep(ctx context.Context, d time.Duration) {
+	ctx, cancel := context.WithTimeout(ctx, d)
+	<-ctx.Done()
+	cancel()
+	return
+}
+
+type readCloserWithCancel struct {
+	io.ReadCloser
+	cancel context.CancelFunc
+}
+
+func (w readCloserWithCancel) Close() error {
+	defer w.cancel()
+	return w.ReadCloser.Close()
+}
+
+func (h HLS) getVariant(u *url.URL) (*m3u8.Variant, error) {
+	r, err := h.Client.Get(u.String())
 	if err != nil {
 		return nil, err
 	} else if r.StatusCode != 200 {
@@ -43,24 +62,48 @@ func getBestBandwidth(u *url.URL) (*m3u8.Variant, error) {
 	master := playlist.(*m3u8.MasterPlaylist)
 
 	log.Printf("found %d variations\n", len(master.Variants))
-	sort.Slice(master.Variants, func(i, j int) bool {
-		return master.Variants[i].Bandwidth > master.Variants[j].Bandwidth
-	})
-
-	for _, v := range master.Variants {
-		if v.Iframe {
-			// EXT-X-I-FRAME-STREAM-INF
-			// Not interested.  They're meant for so-called Trick Play, to get
-			// those little thumbnails when you hover over the seekbar.
-			continue
+	if h.SelectVariant != nil {
+		v := h.SelectVariant(master.Variants)
+		if v != nil {
+			return v, nil
 		}
-		return v, nil
+	} else {
+		for _, v := range master.Variants {
+			return v, nil
+		}
 	}
 	return nil, errors.New("no streams found")
 }
 
-func (h HLS) Get(masterURL *url.URL, dst io.Writer) error {
-	variant, err := getBestBandwidth(masterURL)
+func (h HLS) decodeFrom(ctx context.Context, u *url.URL) (*MediaPlaylist, error) {
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// add a resonable timeout
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	r, err := h.Client.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	} else if r.StatusCode != 200 {
+		return nil, HTTPError{r}
+	}
+
+	playlist, playlistType, err := DecodeFrom(r.Body)
+	if err != nil {
+		return nil, err
+	} else if playlistType != m3u8.MEDIA {
+		return nil, errors.New("expected Media Playlist")
+	}
+
+	return playlist.(*MediaPlaylist), nil
+}
+
+func (h HLS) Download(ctx context.Context, masterURL *url.URL, dst io.Writer) error {
+	variant, err := h.getVariant(masterURL)
 	if err != nil {
 		return err
 	}
@@ -70,144 +113,138 @@ func (h HLS) Get(masterURL *url.URL, dst io.Writer) error {
 		return err
 	}
 
-	seenMediaSequence := uint64(0)
-	byterangeOffsets := make(map[string]int64)
+	g, ctx := errgroup.WithContext(ctx)
 
-	for {
-		log.Println("downloading playlist", variant.URI)
-		r, err := http.Get(variantURL.String())
-		if err != nil {
-			return err
-		} else if r.StatusCode != 200 {
-			return HTTPError{r}
-		}
+	segDataCh := make(chan io.ReadCloser)
+	g.Go(func() error {
+		defer close(segDataCh)
 
-		playlist, playlistType, err := DecodeFrom(r.Body)
-		if err != nil {
-			return err
-		} else if playlistType != m3u8.MEDIA {
-			return errors.New("expected Media Playlist")
-		}
-		media := playlist.(*MediaPlaylist)
+		var seenMediaSequence uint64
+		byterangeOffsets := make(map[string]int64)
 
-		if media.TargetDuration <= 0 {
-			return errors.New("EXT-X-TARGETDURATION was not positive")
-		} else if media.TargetDuration >= 90*time.Second {
-			return errors.New("EXT-X-TARGETDURATION was too long")
-		}
+		for {
+			log.Println("downloading playlist", variant.URI)
 
-		if media.Iframe {
-			// as far as I can tell, non EXT-X-I-FRAME-STREAM-INF Media
-			// Playlists should not be EXT-X-I-FRAMES-ONLY
-			return errors.New("EXT-I-FRAMES-ONLY")
-		}
-
-		lastSegment := media.Segments[len(media.Segments)-1]
-		if seenMediaSequence >= lastSegment.SeqId {
-			// If the client reloads a Playlist file and finds that it has not
-			// changed, then it MUST wait for a period of one-half the target
-			// duration before retrying.
-
-			time.Sleep(media.TargetDuration / 2)
-			continue
-		}
-
-		// When a client loads a Playlist file for the first time or reloads a
-		// Playlist file and finds that it has changed since the last time it
-		// was loaded, the client MUST wait for at least the target duration
-		// before attempting to reload the Playlist file again, measured from
-		// the last time the client began loading the Playlist file.
-		waitCh := time.After(media.TargetDuration)
-
-		segCh := make(chan io.ReadCloser)
-
-		var g errgroup.Group
-		g.Go(func() error {
-			for r := range segCh {
-				if _, err := io.Copy(dst, r); err != nil {
-					return err
-				}
-				if err := r.Close(); err != nil {
-					return err
-				}
+			lastLoadedPlaylist := time.Now()
+			media, err := h.decodeFrom(ctx, variantURL)
+			if err != nil {
+				return err
 			}
-			return nil
-		})
 
-		for _, seg := range media.Segments {
-			if seg.SeqId <= seenMediaSequence {
-				log.Println("skipping segment", seg.URI)
+			if media.TargetDuration <= 0 {
+				return errors.New("EXT-X-TARGETDURATION was not positive")
+			} else if media.TargetDuration >= 90*time.Second {
+				return errors.New("EXT-X-TARGETDURATION was too long")
+			}
+
+			if media.Iframe {
+				return errors.New("EXT-I-FRAMES-ONLY")
+			}
+
+			lastSegment := media.Segments[len(media.Segments)-1]
+			if seenMediaSequence >= lastSegment.SeqId {
+				// If the client reloads a Playlist file and finds that it has not
+				// changed, then it MUST wait for a period of one-half the target
+				// duration before retrying.
+
+				sleep(ctx, media.TargetDuration/2)
 				continue
-			} else if seg.SeqId > seenMediaSequence+1 {
-				log.Println("\033[31m", seg.SeqId-seenMediaSequence-1, "segments expired", "\033[m")
-			}
-			seenMediaSequence = seg.SeqId
-
-			if seg.Key != nil {
-				return errors.New("segment is encrypted")
 			}
 
-			segURL, err := variantURL.Parse(seg.URI)
-			if err != nil {
-				return err
-			}
+			for _, seg := range media.Segments {
+				if seg.SeqId <= seenMediaSequence {
+					log.Println("skipping segment", seg.URI)
+					continue
+				} else if seg.SeqId > seenMediaSequence+1 {
+					log.Println("\033[31m", seg.SeqId-seenMediaSequence-1, "segments expired", "\033[m")
+				}
+				seenMediaSequence = seg.SeqId
 
-			log.Println("downloading segment", seg.URI)
-			req, err := http.NewRequest("GET", segURL.String(), nil)
-			if err != nil {
-				return err
-			}
-
-			if seg.Limit < 0 {
-				return errors.New("EXT-X-BYTERANGE is negative")
-			} else if seg.Limit != 0 {
-				offset, ok := byterangeOffsets[segURL.String()]
-				if seg.Offset != 0 {
-					offset = seg.Offset
-				} else if !ok {
-					return errors.New("EXT-X-BYTERANGE offset not given")
+				if seg.Key != nil {
+					return errors.New("segment is encrypted")
 				}
 
-				// the Range header is inclusive
-				end := offset + seg.Limit - 1
-				req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", offset, end))
-				byterangeOffsets[segURL.String()] = end + 1
-			}
-
-			err = retry(media.TargetDuration, func() error {
-				segData, err := h.Client.Do(req)
-
-				// retry the request if it failed due to network or server issues
+				segURL, err := variantURL.Parse(seg.URI)
 				if err != nil {
 					return err
-				} else if segData.StatusCode != 200 {
-					segData.Body.Close()
-					if segData.StatusCode >= 500 {
-						return HTTPError{segData}
-					} else if segData.StatusCode >= 400 {
-						// client error, the fault is on us
-						return stopRetrying{HTTPError{segData}}
-					}
 				}
 
-				segCh <- segData.Body
+				log.Println("downloading segment", seg.URI)
+				req, err := http.NewRequest("GET", segURL.String(), nil)
+				if err != nil {
+					return err
+				}
+
+				if seg.Limit < 0 {
+					return errors.New("EXT-X-BYTERANGE is negative")
+				} else if seg.Limit != 0 {
+					offset, ok := byterangeOffsets[segURL.String()]
+					if seg.Offset != 0 {
+						offset = seg.Offset
+					} else if !ok {
+						return errors.New("EXT-X-BYTERANGE offset not given")
+					}
+
+					// the Range header is inclusive
+					end := offset + seg.Limit - 1
+					req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", offset, end))
+					byterangeOffsets[segURL.String()] = end + 1
+				}
+
+				ctx, cancel := context.WithTimeout(ctx, 2*media.TargetDuration)
+				segData, err := h.Client.Do(req.WithContext(ctx))
+				if err != nil {
+					cancel()
+					return err
+				}
+
+				segBody := readCloserWithCancel{segData.Body, cancel}
+				if segData.StatusCode != 200 {
+					segBody.Close()
+					return HTTPError{segData}
+				}
+
+				select {
+				case segDataCh <- segBody:
+
+				case <-ctx.Done():
+					segBody.Close()
+					return ctx.Err()
+				}
+			}
+
+			if media.Closed {
 				return nil
-			})
-			if err != nil {
+			}
+
+			// When a client loads a Playlist file for the first time or reloads a
+			// Playlist file and finds that it has changed since the last time it
+			// was loaded, the client MUST wait for at least the target duration
+			// before attempting to reload the Playlist file again, measured from
+			// the last time the client began loading the Playlist file.
+			sleep(ctx, time.Until(lastLoadedPlaylist.Add(media.TargetDuration)))
+		}
+	})
+
+	g.Go(func() error {
+		for r := range segDataCh {
+			// TODO: limit the maximum buffer size
+			var buf bytes.Buffer
+
+			if _, err := io.Copy(&buf, r); err != nil {
+				r.Close()
+				return err
+			}
+			if _, err := io.Copy(dst, &buf); err != nil {
+				r.Close()
+				return err
+			}
+			if err := r.Close(); err != nil {
 				return err
 			}
 		}
+		return nil
+	})
 
-		close(segCh)
-		if err := g.Wait(); err != nil {
-			return err
-		}
-
-		if media.Closed {
-			break
-		}
-		<-waitCh
-	}
-
-	return nil
+	return g.Wait()
 }
