9e2eb54034a741d5d06dd6ba49f33479d4a839c5
Otto Modinos <ottomodinos@gmail.com>
2018-12-30 05:18:54 +0200

    add byterange tests
    this actualy fails

--- hls/hls_test.go
+++ hls/hls_test.go
@@ -31,9 +31,55 @@ func TestEncryption(t *testing.T) {
 	}
 
 	err := h.Download(context.Background(), srv.URL+"/media.m3u8", ioutil.Discard)
-	// TODO: do errors differently
 	if err.Error() == "EXT-X-KEY not supported" {
+		// TODO: add ErrNotSupported
 	} else if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestByterange(t *testing.T) {
+	mux := http.NewServeMux()
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.HandleFunc("/media.m3u8", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		io.WriteString(w, `
+			#EXTM3U
+			#EXT-X-VERSION:4
+			#EXT-X-TARGETDURATION:10
+			#EXT-X-BYTERANGE:1000@0
+			#EXTINF:9.0,
+			video.ts
+			#EXTINF:9.0,
+			#EXT-X-BYTERANGE:1000
+			video.ts
+			#EXT-X-ENDLIST
+		`)
+	})
+
+	var mediaSequence uint64
+	mux.HandleFunc("/video.ts", func(w http.ResponseWriter, r *http.Request) {
+		httpRange := r.Header.Get("Range")
+		switch {
+		case mediaSequence == 0 && httpRange == "bytes=1-1000":
+		case mediaSequence == 1 && httpRange == "bytes=1001-2000":
+		default:
+			t.Error("requested wrong range", httpRange)
+		}
+
+		w.WriteHeader(206)
+		w.Write(make([]byte, 1000))
+		mediaSequence++ // FIXME: this is not thread safe
+	})
+
+	h := Client{
+		Client: srv.Client(),
+	}
+
+	err := h.Download(context.Background(), srv.URL+"/media.m3u8", ioutil.Discard)
+	if err != nil {
 		t.Fatal(err)
 	}
 }
