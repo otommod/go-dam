e632b56bb71d0fc813a31be913ac0e399c5c48af
Otto Modinos <ottomodinos@gmail.com>
2018-12-30 06:12:32 +0200

    add segment skipping tests

--- hls/hls_test.go
+++ hls/hls_test.go
@@ -9,6 +9,50 @@ import (
 	"testing"
 )
 
+func TestMediaSequence(t *testing.T) {
+	mux := http.NewServeMux()
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	accessed := make(map[string]int)
+	mux.HandleFunc("/media.m3u8", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		io.WriteString(w, `
+			#EXTM3U
+			#EXT-X-VERSION:3
+			#EXT-X-TARGETDURATION:4
+			#EXTINF:3.14,
+			seg/1.ts
+			#EXTINF:3.14,
+			seg/2.ts
+		`)
+
+		if accessed[r.URL.EscapedPath()] >= 2 {
+			io.WriteString(w, "#EXT-X-ENDLIST")
+		}
+		accessed[r.URL.EscapedPath()]++
+	})
+
+	mux.HandleFunc("/seg/", func(w http.ResponseWriter, r *http.Request) {
+		if accessed[r.URL.EscapedPath()] > 1 {
+			t.Error("segment was not skipped like it should have", r.URL.EscapedPath())
+		}
+
+		w.WriteHeader(200)
+		w.Write(make([]byte, 1000))
+		accessed[r.URL.EscapedPath()]++
+	})
+
+	h := Client{
+		Client: srv.Client(),
+	}
+
+	err := h.Download(context.Background(), srv.URL+"/media.m3u8", ioutil.Discard)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestEncryption(t *testing.T) {
 	mux := http.NewServeMux()
 	srv := httptest.NewServer(mux)
@@ -19,9 +63,9 @@ func TestEncryption(t *testing.T) {
 		io.WriteString(w, `
 			#EXTM3U
 			#EXT-X-VERSION:6
-			#EXT-X-TARGETDURATION:10
+			#EXT-X-TARGETDURATION:4
 			#EXT-X-KEY:METHOD=AES-128,URI="key"
-			#EXTINF:9.0,
+			#EXTINF:3.14,
 			seg.ts
 		`)
 	})
@@ -48,11 +92,11 @@ func TestByterange(t *testing.T) {
 		io.WriteString(w, `
 			#EXTM3U
 			#EXT-X-VERSION:4
-			#EXT-X-TARGETDURATION:10
+			#EXT-X-TARGETDURATION:4
 			#EXT-X-BYTERANGE:1000@0
-			#EXTINF:9.0,
+			#EXTINF:3.14,
 			video.ts
-			#EXTINF:9.0,
+			#EXTINF:3.14,
 			#EXT-X-BYTERANGE:1000
 			video.ts
 			#EXT-X-ENDLIST
